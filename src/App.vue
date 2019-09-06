29c26ec3e14290bcfb81508336c5fd900279b590
theimberger <theimberger@gmail.com>
2019-09-05 20:35:23 -0400

    Create files for game

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Game v-bind:score='score' />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Game from './components/game.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    Game
+  },
+  data: () => {
+    return {
+      score: 0,
+    }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+
 }
 </style>
---
+++ src/components/game.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Tile v-bind:color='color' />
+  </div>
+</template>
+
+<script>
+import Tile from './tile.vue'
+
+export default {
+  name: 'Game',
+  components: {
+    Tile
+  },
+  props: {
+    score: Number,
+  },
+  data: () => {
+    return {
+      color: 'hello'
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
