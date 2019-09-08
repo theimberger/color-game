9ec46ebfea6f6aa382307a6e22651d3a497566d6
theimberger <theimberger@gmail.com>
2019-09-08 16:42:19 -0400

    Add lighter color

--- src/components/game.vue
+++ src/components/game.vue
@@ -1,34 +1,73 @@
 <template>
-  <div>
-    <Tile v-for='n in 10' v-bind:style='colorStyle' v-bind:key=n />
+  <div class='game'>
+    <div class='board'>
+      <div class='tile' v-for='n in tileCount' v-bind:style='colorStyle' v-bind:key=n />
+    </div>
   </div>
 </template>
 
 <script>
-import Tile from './tile.vue'
 
 export default {
   name: 'Game',
-  components: {
-    Tile
-  },
+  components: {},
   props: {
     score: Number,
   },
-  data: () => {
+  data: function() {
+    const rowLength = (Math.floor(this.score ** .5) + 2)
+    const factor = rowLength + 5
+    const tileCount = rowLength ** 2
+
     const color = []
     for(let i = 0; i < 3; i += 1) {
       color.push(15 + Math.floor(Math.random() * 200))
     }
+    
+    const lighter = color.map(val => val + factor)
+    const tileWidth = (300 - (6 * rowLength)) / rowLength
+
+    const correctTile = Math.floor(Math.random() * tileCount) + 1
+
     return {
       colorStyle: {
-        background: `rgb(${color.join(', ')})`
+        background: `rgb(${color.join(', ')})`,
+        height: `${tileWidth}px`,
+        width: `${tileWidth}px`,
       },
+      lightColor: {
+        background: `rgb(${lighter.join(', ')})`
+      },
+      tileCount,
+      correctTile,
     }
   },
 }
 </script>
 
 <style scoped>
+  .board, .game {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    left: 0;
+    top: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 
+  .board {
+    height: auto;
+    width: 300px;
+    position: relative;
+    flex-wrap: wrap;
+  }
+
+  .tile {
+    display: block;
+    margin: 3px;
+    border-radius: 10px;
+    flex-shrink: 0;
+  }
 </style>
